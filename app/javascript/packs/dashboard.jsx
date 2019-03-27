7136c94354ec61dbf48fce6c23a7a0045d256307
C_Groff, Jason <C_Groff.Jason@Cerner.com>
2019-03-27 16:01:49 -0500

    Logged in link state

--- app/javascript/packs/dashboard.jsx
+++ app/javascript/packs/dashboard.jsx
@@ -5,6 +5,21 @@ import 'bootstrap/dist/css/bootstrap.css';
 class Dashboard extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      user: {},
+      loggedIn: false
+    };
+  }
+
+  componentDidMount() {
+    if (window.location.hash) {
+      let fitbitToken = window.location.hash
+        .slice(1)
+        .split('&')[0]
+        .replace('access_token=', '');
+      console.log(fitbitToken);
+      this.setState({ loggedIn: true });
+    }
   }
 
   render() {
@@ -15,11 +30,13 @@ class Dashboard extends Component {
           <p className="lead">Your personal fitness dashboard</p>
         </header>
 
-        <div className="text-center">
-          <a href="https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=22DLD4&redirect_uri=http%3A%2F%localhost%3A3001%2Ffitbit_auth&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800">
-            Log in with FitBit
-          </a>
-        </div>
+        {!this.state.loggedIn && (
+          <div className="text-center">
+            <a href="https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22DLD4&redirect_uri=http%3A%2F%localhost%3A3001%2Ffitbit_auth&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800">
+              Log in with FitBit
+            </a>
+          </div>
+        )}
       </div>
     );
   }
