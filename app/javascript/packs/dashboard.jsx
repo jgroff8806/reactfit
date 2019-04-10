664e3847cc61d2313e8f75ea1ad4e4e0bca24939
C_Groff, Jason <C_Groff.Jason@Cerner.com>
2019-04-10 16:04:18 -0500

    Created lifetimeStats component and integrated lifetimeActivity API call

--- app/javascript/packs/dashboard.jsx
+++ app/javascript/packs/dashboard.jsx
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
+import LifetimeStats from './lifetimeStats';
 
 class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
       user: {},
-      loggedIn: false
+      loggedIn: false,
+      lifetimeBest: { steps: '', distance: '' },
+      lifetimeTotals: { steps: '', distance: '' }
     };
   }
 
@@ -31,6 +34,18 @@ class Dashboard extends Component {
           this.setState({ user: response.data.user, loggedIn: true });
         })
         .catch(error => console.log(error));
+
+      axios({
+        method: 'get',
+        url: 'https://api.fitbit.com/1/user/-/activities.json',
+        headers: { Authorization: 'Bearer ' + fitbitToken },
+        mode: 'cors'
+      })
+        .then(response => {
+          console.log(response);
+          this.setState({ lifetimeBest: response.data.best.total, lifetimeTotals: response.data.lifetime.total });
+        })
+        .catch(error => console.log(error));
     }
   }
 
@@ -45,11 +60,40 @@ class Dashboard extends Component {
 
         {!this.state.loggedIn && (
           <div className="text-center">
-            <a href="https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22DLD4&redirect_uri=http%3A%2F%localhost%3A3001%2Ffitbit_auth&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800">
+            <a href="https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22DLD4&redirect_uri=http%3A%2F%localhost%3A8000%2Ffitbit_auth&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800">
               Log in with FitBit
             </a>
           </div>
         )}
+
+        <div className="row">
+          <div className="col-lg-3">
+            <LifetimeStats lifetimeTotals={this.state.lifetimeTotals} lifetimeBest={this.state.lifetimeBest} />
+
+            <div className="card">
+              <div className="card-header">
+                <h4>Badges</h4>
+              </div>
+              <div className="card-body" />
+            </div>
+          </div>
+
+          <div className="col-lg-6">
+            <div className="card">
+              <div className="card-header">Steps</div>
+            </div>
+
+            <div className="card">
+              <div className="card-header">Distance (miles)</div>
+            </div>
+          </div>
+
+          <div className="col-lg-2 col-lg-offset-1">
+            <div className="card">
+              <div className="card-header">Your Friends</div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ app/javascript/packs/lifetimeStats.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const LifetimeStats = ({ lifetimeTotals, lifetimeBest }) => (
+  <div className="card">
+    <div className="card-header">
+      <h4>Lifetime Stats</h4>
+    </div>
+    <div className="card-body">
+      <h4>Distance</h4>
+      <p>Total: {lifetimeTotals.distance} miles</p>
+      <p>
+        Best: {lifetimeBest.distance.value} on {lifetimeBest.distance.date}
+      </p>
+      <h4>Steps</h4>
+      <p>Total: {lifetimeTotals.steps}</p>
+      <p>
+        Best: {lifetimeBest.steps.value} on {lifetimeBest.steps.date}
+      </p>
+    </div>
+  </div>
+);
+
+export default LifetimeStats;
